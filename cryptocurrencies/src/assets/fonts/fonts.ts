bee526cbd65f103cf536479f8de0cb47f81ff9d2
Sidney Roberto <sidney.sousa@ifms.edu.br>
2022-09-14 20:32:36 -0400

    adicionando o projeto cryptocurrencies

---
+++ cryptocurrencies/src/assets/fonts/fonts.ts
@@ -0,0 +1,7 @@
+import { createGlobalStyle } from 'styled-components'
+import BoldFont from './Poppins-Bold.ttf'
+import RegularFont from './Poppins-Regular.ttf'
+
+const FontStyles = createGlobalStyle`
+    
+`
